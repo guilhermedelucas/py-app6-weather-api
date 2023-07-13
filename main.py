2597ab3d0303126b6d2d827a8a38ec8b3cc5bb18
guilhermedelucas <gdelucasde@gmail.com>
2023-07-13 18:38:35 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template
+import pandas
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/api/v1/<station>/<date>")
+def about(station, date):
+    df = pandas.read_csv("")
+    temperature = df.station(date)
+    return {
+        "station": station,
+        "date": date,
+        "temperature": temperature
+    }
+
+if __name__ == "__main__"
+    app.run(debug=True)
+
+app.run()
