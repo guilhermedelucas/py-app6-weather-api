4c2a9956f5caedf909de1a7afd7d53ac8bef9df8
guilhermedelucas <gdelucasde@gmail.com>
2023-07-14 17:58:56 +0200

    Implement the API to fetch the station+date temperatures

--- main.py
+++ main.py
@@ -8,15 +8,17 @@ app = Flask(__name__)
 def home():
     return render_template("home.html")
 
+
 @app.route("/api/v1/<station>/<date>")
 def about(station, date):
-    df = pandas.read_csv("")
-    temperature = df.station(date)
+    df = pandas.read_csv(f"data_small/TG_STAID{str(station).zfill(6)}.txt", skiprows=20, parse_dates=['    DATE'])
+    temperature = df.loc[df['    DATE'] == date]['   TG'].squeeze() / 10
     return {
         "station": station,
         "date": date,
         "temperature": temperature
     }
+
 
 if __name__ == "__main__":
     app.run(debug=True, port=5001)
