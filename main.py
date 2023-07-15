a29c79ea131fed0912df70af682d6eab0177a114
guilhermedelucas <gdelucasde@gmail.com>
2023-07-15 05:44:48 +0200

    Small refactor, renamed columns

--- main.py
+++ main.py
@@ -12,17 +12,30 @@ def home():
     return render_template("home.html", data=stations.to_html())
 
 
+def get_station_data(station, skiprows=20, parse_dates=True):
+    if parse_dates:
+        df = pandas.read_csv(f"data_small/TG_STAID{str(station).zfill(6)}.txt", skiprows=skiprows, parse_dates=['    DATE'])
+    else:
+        df = pandas.read_csv(f"data_small/TG_STAID{str(station).zfill(6)}.txt", skiprows=skiprows)
+    df.rename(columns={
+        '    DATE': 'DATE',
+        '   TG': 'TG',
+        ' Q_TG': 'Q_TG',
+        ' SOUID': 'SOUID',
+    }, inplace=True)
+    return df
+
 @app.route("/api/v1/<station>")
 def data(station):
-    df = pandas.read_csv(f"data_small/TG_STAID{str(station).zfill(6)}.txt", skiprows=20, parse_dates=['    DATE'])
+    df = get_station_data(station, skiprows=20)
     result = df.to_dict(orient="records")
     return result
 
 
 @app.route("/api/v1/<station>/<date>")
 def about(station, date):
-    df = pandas.read_csv(f"data_small/TG_STAID{str(station).zfill(6)}.txt", skiprows=20, parse_dates=['    DATE'])
-    temperature = df.loc[df['    DATE'] == date]['   TG'].squeeze() / 10
+    df = get_station_data(station, skiprows=20)
+    temperature = df.loc[df['DATE'] == date]['TG'].squeeze() / 10
     return {
         "station": station,
         "date": date,
@@ -31,10 +44,11 @@ def about(station, date):
 
 
 @app.route("/api/v1/year/<station>/<year>")
-def data(station, year):
-    df = pandas.read_csv(f"data_small/TG_STAID{str(station).zfill(6)}.txt", skiprows=20)
-    df['    DATE'] = df['    DATE'].astype(str)
-    result = df[df['    DATE'].startswith(str(year))].to_dict(orient="records")
+def yearly(station, year):
+    df = get_station_data(station, skiprows=20, parse_dates=False)
+    print(df)
+    df['DATE'] = df['DATE'].astype(str)
+    result = df[df['DATE'].str.startswith(str(year))].to_dict(orient="records")
     return result
 
 
